d877e0d775749c30ee85dfb70ed5b4e875a9a4f1
yueheng-li <chunhui.li@outlook.com>
2017-06-04 15:29:44 +0800

    first commit

---
+++ elecom-dao/src/main/java/com/elecom/crawler/entity/Product.java
@@ -0,0 +1,20 @@
+package com.elecom.crawler.entity;
+
+public class Product extends Entity{
+	
+	private String product_no;
+	private String product_name;
+	public String getProduct_no() {
+		return product_no;
+	}
+	public void setProduct_no(String product_no) {
+		this.product_no = product_no;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	
+}
---
+++ elecom-dao/src/main/java/com/elecom/crawler/entity/ProductBaseInfo.java
@@ -0,0 +1,42 @@
+package com.elecom.crawler.entity;
+
+public class ProductBaseInfo {
+
+	private String product_no;
+	private String product_name;
+	private String shop_id;
+	private String shop_name;
+	private String product_price;
+	public String getProduct_no() {
+		return product_no;
+	}
+	public void setProduct_no(String product_no) {
+		this.product_no = product_no;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getShop_name() {
+		return shop_name;
+	}
+	public void setShop_name(String shop_name) {
+		this.shop_name = shop_name;
+	}
+	public String getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(String product_price) {
+		this.product_price = product_price;
+	}
+	public String getShop_id() {
+		return shop_id;
+	}
+	public void setShop_id(String shop_id) {
+		this.shop_id = shop_id;
+	}
+	
+	
+}
